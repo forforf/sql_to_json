391c9bb2a845b7c9cc589d5a2d1fcfc6e2106b6e
Dave <dmarti21@gmail.com>
2012-12-06 22:04:26 -0500

    enables session in server

--- server.rb
+++ server.rb
@@ -1,8 +1,10 @@
 require 'sinatra/base'
 
+
 class Server < Sinatra::Base
   set :static, true
   set :root, APP_ROOT #set in config.ru
+  enable :sessions
 
   get '/' do
     "Root - Running, but no data"
